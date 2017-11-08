939941d935e7c7d2cc0d960b94645792c0ed1e95
Kristoffel Pirard <kristoffel.pirard@tomra.com>
2017-11-08 12:21:34 +0100

    add

--- monadic-clutter/monadicclutter.cpp
+++ monadic-clutter/monadicclutter.cpp
@@ -3,9 +3,41 @@
 
 #include "stdafx.h"
 
+#include <cassert>
+#include <optional>
+#include <string>
+#include <string_view>
+#include <vector>
+#include <iostream>
 
-int main()
+
+struct FormInput { std::string_view value; }; 
+struct Index { int value; };
+struct Ratio { double value; };
+
+struct Voltage { double value; };
+struct VoltageRange { Voltage high; Voltage low; };
+
+Index fromForm(FormInput input) { return { atoi(input.value.data()) }; }
+Ratio fromIndex(Index i) {
+    return { static_cast<double>(i.value) / 100.0 };
+}
+auto inRange(VoltageRange range) {
+    return [=](Ratio r) { return Voltage{ range.low.value + r.value * (range.high.value - range.low.value) }; };
+}
+const VoltageRange range{ { 1.0 },{ 10.0 } };
+const auto toVoltage = inRange(range);
+
+auto toVoltageString(const std::vector<std::string_view> &args)
 {
+    const auto input = FormInput{ args.at(1) };
+    auto v = toVoltage(fromIndex(fromForm(input)));
+    return std::to_string(v.value).substr(0, 3) + "V";
+}
+
+int main(const char** args, const int argc)
+{
+    assert(toVoltageString({ "90" }) == "1.9V");
     return 0;
 }
 
