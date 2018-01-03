4994d6ea62ec64b8fd795733a8a7f109b8872c29
xtofl <kristoffel.pirard@gmail.com>
2018-01-03 17:13:27 +0100

    [check] out of range

--- monadic-clutter/monadicclutter.cpp
+++ monadic-clutter/monadicclutter.cpp
@@ -27,7 +27,10 @@ Ratio fromIndex(Index i) {
     return { static_cast<double>(i.value) / 100.0 };
 }
 auto inRange(VoltageRange range) {
-    return [=](Ratio r) { return Voltage{ range.low.value + r.value * (range.high.value - range.low.value) }; };
+    return [=](Ratio r) {
+        if (r.value < 0.0 || 1.0 < r.value) return std::optional<Voltage>{};
+        return std::make_optional(Voltage{ range.low.value + r.value * (range.high.value - range.low.value) });
+    };
 }
 const VoltageRange range{ { 1.0 },{ 10.0 } };
 const auto toVoltage = inRange(range);
@@ -39,7 +42,8 @@ auto toVoltageString(const std::optional<std::string_view> &arg)
     auto index = fromForm(input);
     if (!index) return std::string{"?"};
     auto v = toVoltage(fromIndex(*index));
-    return std::to_string(v.value).substr(0, 3) + "V";
+    if (!v) return std::string{"?"};
+    return std::to_string(v->value).substr(0, 3) + "V";
 }
 
 int main(const int argc, const char** args)
@@ -49,6 +53,7 @@ int main(const int argc, const char** args)
     std::cout << toVoltageString(arg) << std::endl;
     assert(toVoltageString({}) == "?");
     assert(toVoltageString({ "ZORK" }) == "?");
+    assert(toVoltageString({ "110" }) == "?");
     assert(toVoltageString({ "90" }) == "9.1V");
     return 0;
 }
