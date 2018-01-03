ffde8e4d4325acebd40f21ed6bae3d71b835fe32
xtofl <kristoffel.pirard@gmail.com>
2018-01-03 16:41:32 +0100

    simple to voltage convertor

--- monadic-clutter/monadicclutter.cpp
+++ monadic-clutter/monadicclutter.cpp
@@ -1,27 +1,22 @@
+// monadicclutter.cpp : Defines the entry point for the console application.
+//
+
+#include <cassert>
 #include <optional>
 #include <string>
 #include <string_view>
 #include <vector>
 #include <iostream>
-#include <type_traits>
-
-#define assert(x) if(!(x)) { std::cerr << "fail: " << #x << "\n"; return 1; } \
-else { std::cout << "OK: " << #x << "\n"; }
 
 
 struct FormInput { std::string_view value; }; 
-struct Index { long value; };
+struct Index { int value; };
 struct Ratio { double value; };
 
 struct Voltage { double value; };
-struct VoltageRange { Voltage high; Voltage low; };
+struct VoltageRange { Voltage low; Voltage high; };
 
-std::optional<Index> fromForm(FormInput input) {
-    char *end;
-    auto i = strtol(input.value.data(), &end, 10);
-    if (end == input.value.data()) return {};
-    else return { { i } };
-}
+Index fromForm(FormInput input) { return { atoi(input.value.data()) }; }
 Ratio fromIndex(Index i) {
     return { static_cast<double>(i.value) / 100.0 };
 }
@@ -30,39 +25,18 @@ auto inRange(VoltageRange range) {
 }
 const VoltageRange range{ { 1.0 },{ 10.0 } };
 const auto toVoltage = inRange(range);
-namespace safe {
-    auto inRange(VoltageRange range) {
-        auto isSafe = [=](Ratio r) { return 0 <= r.value && r.value <= 1; };
-        auto unChecked = ::inRange(range);
-        return [=](Ratio r) -> std::optional<Voltage>{
-            if (!isSafe(r)) return {};
-            else return { unChecked(r) };
-        };
-    }
-    const auto toVoltage = safe::inRange(range);
+
+auto toVoltageString(const std::vector<std::string_view> &args)
+{
+    const auto input = FormInput{ args.at(0) };
+    auto v = toVoltage(fromIndex(fromForm(input)));
+    return std::to_string(v.value).substr(0, 3) + "V";
 }
 
-std::string toVoltageString(const std::vector<std::string_view> &args)
+int main(const int argc, const char** args)
 {
-    if (args.size() >= 1) {
-        const auto input = FormInput{ args.at(0) };
-        const auto index = fromForm(input);
-        if (index) {
-            auto v = safe::toVoltage(fromIndex(*index));
-            if (v) {
-                return std::to_string(v->value).substr(0, 3) + "V";
-            }
-        }
-    }
-    return "?";
-}
-
-int main(int argc, const char** args)
-{
-    assert(toVoltageString({ "90" }) == "1.9V");
-    assert(toVoltageString({ }) == "?");
-    assert(toVoltageString({ "not a number" }) == "?");
-    assert(toVoltageString({ "200" }) == "?"); // out of bounds
+    std::cout << toVoltageString({args[1]}) << std::endl;
+    assert(toVoltageString({ "90" }) == "9.1V");
     return 0;
 }
 
