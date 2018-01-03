c35bbf25cfadd5493c949d32cc2889b4680ae92e
xtofl <kristoffel.pirard@gmail.com>
2018-01-03 20:58:36 +0100

    WIP composition

--- monadic-clutter/monadicclutter.cpp
+++ monadic-clutter/monadicclutter.cpp
@@ -22,8 +22,8 @@ std::optional<Index> fromForm(FormInput input) {
         return {};
     }
 };
-Ratio fromIndex(Index i) {
-    return { static_cast<double>(i.value) / 100.0 };
+std::optional<Ratio> fromIndex(Index i) {
+    return {Ratio{static_cast<double>(i.value) / 100.0}};
 }
 auto inRange(VoltageRange range) {
     return [=](Ratio r) {
@@ -34,29 +34,47 @@ auto inRange(VoltageRange range) {
 const VoltageRange range{ { 1.0 },{ 10.0 } };
 const auto toVoltage = inRange(range);
 
-auto toVoltageString(const std::optional<std::string_view> &arg)
-{
-    if (arg) {
-        auto index = fromForm({ *arg });
-        if (index) {
-            auto v = toVoltage(fromIndex(*index));
-            if (v) {
-                return std::to_string(v->value).substr(0, 3) + "V";
-            }
-        }
-    }
-    return std::string{"?"};
- }
+template<typename F, typename...Fs>
+auto compose(F f, Fs ...fs);
+
+template<typename F>
+auto compose(F f) { return f; }
+
+template<typename F, typename...Fs>
+auto compose(F f, Fs ...fs) {
+        auto rest = compose(fs...);
+        return [=](auto x){
+            auto fx = f(x);
+            using T = decltype(rest(*fx));
+            return fx ? rest(*fx) : T{};
+        };
+}
+
+auto voltageToString(Voltage v) { return std::to_string(v.value).substr(0, 3) + "V"; };
+std::optional<FormInput> fromArgument(std::optional<std::string_view> s) {
+    if(s)
+        return {FormInput{*s}};
+    else
+        return {};
+}
+
+// auto toVoltageString = compose(
+//     fromArgument,
+//     fromForm,
+//     fromIndex,
+//     toVoltage,
+//     voltageToString);
 
 int main(const int argc, const char** args)
 {
     std::optional<std::string_view> arg;
     if (argc > 1) arg = {args[1]};
-    std::cout << toVoltageString(arg) << std::endl;
-    assert(toVoltageString({}) == "?");
-    assert(toVoltageString({ "ZORK" }) == "?");
-    assert(toVoltageString({ "110" }) == "?");
-    assert(toVoltageString({ "90" }) == "9.1V");
+    compose(fromArgument, fromForm)(arg);
+    // std::cout << toVoltageString(arg) << std::endl;
+    // assert(toVoltageString({}) == "?");
+    // assert(toVoltageString({ "ZORK" }) == "?");
+    // assert(toVoltageString({ "110" }) == "?");
+    // assert(toVoltageString({ "90" }) == "9.1V");
     return 0;
 }
 
