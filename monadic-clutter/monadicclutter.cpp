26fa48d635b896ac33ac0e1e00f14112624849a9
xtofl <kristoffel.pirard@gmail.com>
2018-01-16 21:27:59 +0100

    add optional conversion, and need flatten

--- monadic-clutter/monadicclutter.cpp
+++ monadic-clutter/monadicclutter.cpp
@@ -16,8 +16,12 @@ struct Ratio { double value; };
 struct Voltage { double value; };
 struct VoltageRange { Voltage low; Voltage high; };
 
-Index fromForm(FormInput input) {
-    return { std::stoi(input.value.data()) };
+std::optional<Index> fromForm(FormInput input) {
+    try {
+        return  {{ std::stoi(input.value.data()) }};
+    } catch( const std::invalid_argument &) {
+        return {};
+    }
 }
 Ratio fromIndex(Index i) {
     return { static_cast<double>(i.value) / 100.0 };
@@ -28,6 +32,12 @@ auto inRange(VoltageRange range) {
 const VoltageRange range{ { 1.0 },{ 10.0 } };
 const auto toVoltage = inRange(range);
 
+template<typename T>
+auto flatten(const std::optional<std::optional<T>> &arg) -> std::optional<T> {
+    if (arg) return *arg;
+    else return {};
+}
+
 template<typename T, typename Ft>
 auto transform(const std::optional<T> &arg, Ft f) -> std::optional< decltype(f(*arg)) >
 {
@@ -35,12 +45,12 @@ auto transform(const std::optional<T> &arg, Ft f) -> std::optional< decltype(f(*
     else return {};
 }
 
-Voltage stringToVoltage(const std::string_view arg)
+std::optional<Voltage> stringToVoltage(const std::string_view arg)
 {
     const auto input = FormInput{ arg };
     const auto index = fromForm(input);
-    const auto ratio = fromIndex(index);
-    const auto voltage = toVoltage(ratio);
+    const auto ratio = transform(index, fromIndex);
+    const auto voltage = transform(ratio, toVoltage);
     return voltage;
 }
 
@@ -52,7 +62,7 @@ int main(const int argc, const char** args)
 {
     std::optional<std::string_view> arg;
     if(argc > 1) arg = args[1];
-    const auto voltage = transform(arg, stringToVoltage);
+    const auto voltage = flatten(transform(arg, stringToVoltage));
     std::cout << transform(voltage, voltageToString).value_or("?") << std::endl;
     return 0;
 }
