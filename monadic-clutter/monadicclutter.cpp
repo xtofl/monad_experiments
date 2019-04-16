2813bc7febc8213bde8e36374c232b1a5220d8c7
xtofl <kristoffel.pirard@gmail.com>
2019-04-16 08:11:32 +0200

    Make it build with cmake, on linux, without stdafx

--- monadic-clutter/monadicclutter.cpp
+++ monadic-clutter/monadicclutter.cpp
@@ -1,14 +1,14 @@
 // monadicclutter.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
-
 #include <cassert>
 #include <optional>
 #include <string>
 #include <string_view>
 #include <vector>
 #include <iostream>
+#include <algorithm>
+#include <sstream>
 
 
 struct FormInput { std::string_view value; }; 
@@ -28,15 +28,25 @@ auto inRange(VoltageRange range) {
 const VoltageRange range{ { 1.0 },{ 10.0 } };
 const auto toVoltage = inRange(range);
 
+auto precision(int p, double d) {
+    std::ostringstream ss;
+    ss.precision(2);
+    ss << d;
+    return ss.str();
+}
+
 auto toVoltageString(const std::vector<std::string_view> &args)
 {
     const auto input = FormInput{ args.at(1) };
     auto v = toVoltage(fromIndex(fromForm(input)));
-    return std::to_string(v.value).substr(0, 3) + "V";
+    return precision(2, v.value) + "V";
 }
 
-int main(const char** args, const int argc)
+int main(const int argc, const char** arg)
 {
+    std::vector<std::string_view> args;
+    std::copy_n(arg, argc, std::back_inserter(args));
+    puts(toVoltageString(args).c_str());
     assert(toVoltageString({ "90" }) == "1.9V");
     assert(toVoltageString({ }) == "?");
     return 0;
